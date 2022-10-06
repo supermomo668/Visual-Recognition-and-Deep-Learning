9d7606cb0d4b3f73a6e49be0697e5bab147587f0
supermo668 <matthew@Biotrillion.com>
2022-10-06 13:01:44 -0400

    updated task 2

--- hw1-object_localization/.ipynb_checkpoints/task_2-checkpoint.py
+++ hw1-object_localization/.ipynb_checkpoints/task_2-checkpoint.py
@@ -89,14 +89,44 @@ output_dir = "./"
 if not os.path.exists(output_dir):
     os.makedirs(output_dir)
 
+def metric1(output, target):
+    # TODO (Q1.5): compute metric1
+    target = target.detach().numpy().astype('int')
+    output = output.detach().numpy().astype('float')
+    # get features that aren't all zero
+    feat_considered = ~np.all(np.concatenate([target, output]), axis=0)
+    mean_ap = sklearn.metrics.average_precision_score(target[:,feat_considered], output[:,feat_considered], average='samples')
+    return mean_ap    #[0]
 
-def calculate_map():
+def calculate_map(roi_bboxes, box_scores,  gt_classes, gt_bboxes, n_classes=20):
     """
     Calculate the mAP for classification.
+    #        
     """
     # TODO (Q2.3): Calculate mAP on test set.
-    # Feel free to write necessary function parameters.
-    pass
+    # Using IOU to iterate each box for each class
+    # Compare each iteratively and take Maximum IOU
+    pred_boxes, gt_boxes = np.asarray(roi_bboxes), np.asarray(gt_bboxes)
+    per_class_iou = np.zeros(n_classes)   # ( 20)
+    filtered_bboxes = []
+    cls_labels = []
+    for class_num in range(20):   # (per class)
+        gt_class_idx = np.where(np.asarray(gt_classes)==class_num)[0]
+        pred_class_idx = np.where(np.argmax(np.asarray(roi_bboxes), axis=1)==class_num)[0]
+        if len(pred_class_idx)==0 or len(gt_class_idx)==0:
+            per_class_iou[class_num] = 0
+            continue
+        pred_class_bbox, gt_class_bbox = pred_boxes[pred_class_idx], gt_boxes[gt_class_idx]
+        #
+        bboxes, conf_scores = nms(pred_class_bbox, box_scores[pred_class_idx])
+        filtered_bboxes.append(bboxes)
+        cls_labels += [class_num]*len(bboxes)
+        # iterate each bbox to gt bbox
+        class_iou = []
+        for i, pred_box in enumerate(pred_class_bbox):
+            class_iou.append(np.max([iou(pred_box, gt_box) for gt_box in gt_class_bbox]))
+        per_class_iou[class_num] = np.array(class_iou).mean()
+    return per_class_iou, (filtered_bboxes, cls_labels)
 
 
 def test_model(model, val_loader=None, thresh=0.05):
@@ -104,28 +134,60 @@ def test_model(model, val_loader=None, thresh=0.05):
     Tests the networks and visualizes the detections
     :param thresh: Confidence threshold
     """
+    test_loss = 0
+    class_id_to_label = dict(enumerate(dataset.CLASS_NAMES))
+    class_ap_table = wandb.Table(columns=dataset.CLASS_NAMES)
+    vis_table = wandb.Table(columns=["image", "prediction", "mean class scores"])
+    batch_class_ap = []
+    #
     with torch.no_grad():
-        for iter, data in enumerate(val_loader):
-
+        batch_ap_by_class = []
+        for i, data in enumerate(val_loader):
             # one batch = data for one image
             image = data['image']
             target = data['label']
             wgt = data['wgt']
             rois = data['rois']
             gt_boxes = data['gt_boxes']
-            gt_class_list = data['gt_classes']
+            gt_class = data['gt_classes']
+            if np.any([len(r)!= 300 for r in rois]):
+                continue
+            print(np.any([len(r)!= 300 for r in rois]), [len(r) for r in rois])
+            img_info = dict.fromkeys(np.arange(args.batch_size), {'pred_boxes':[],'pred_cls':[]})
             # TODO (Q2.3): perform forward pass, compute cls_probs
+            cls_prob = model(image, rois, target)   # (N, 300, 20)
             
+            loss = model.build_loss(cls_prob, target)
+            test_loss += loss.item()
             
             # TODO (Q2.3): Iterate over each class (follow comments)
-            for class_num in range(20):
-                # get valid rois and cls_scores based on thresh
-                
-                # use NMS to get boxes and scores
-                pass
-
+            for n in range(args.batch_size):
+                # Get NMS bboxes
+                ap_by_class, (pred_boxes, labels) = calculate_map(rois[n], cls_prob[n].detach().cpu(),  gt_class[n], gt_boxes[n])
+                batch_class_ap.append(ap_by_class)
+                pred_boxes.append(pred_boxes)
+            running_mean_ap = np.mean(np.stack(batch_class_ap), axis=0)
+            class_ap_table.add_data(*running_mean_ap)
             # TODO (Q2.3): visualize bounding box predictions when required
-            calculate_map()
+            if i%args.val_interval==0:
+                for n, im in enumerate(image):
+                    gtbbox_img = wandb.Image(im, boxes={
+                        "predictions": {
+                        "box_data": get_box_data(data['gt_classes'][n], data['gt_boxes'][n]),
+                        "class_labels": class_id_to_label,       
+                        },
+                    })
+                    predbbox_img = wandb.Image(im, boxes={
+                        "predictions": {
+                        "box_data": get_box_data(img_info[n]['pred_cls'], img_info[n]['pred_boxes']),
+                        "class_labels": class_id_to_label,       
+                        },
+                    })
+                    vis_table.add_data(wandb.Image(im), gtbbox_img, predbbox_img)    
+        wandb.log({"Visuals": vis_table})
+        wandb.log({"AP by Class": class_ap_table})
+    return np.nanmean(np.stack(batch_class_ap), axis=0)
+            
 
 
 def train_model(model, train_loader=None, val_loader=None, optimizer=None, args=None):
@@ -135,9 +197,12 @@ def train_model(model, train_loader=None, val_loader=None, optimizer=None, args=
     # Initialize training variables
     train_loss = 0
     step_cnt = 0
+    class_id_to_label = dict(enumerate(dataset.CLASS_NAMES))
+    class_ap_table = wandb.Table(columns=[str(c) for c in np.arange(len(dataset.CLASS_NAMES))])
+    vis_table = wandb.Table(columns=["image", "class AP scores"])
     for epoch in range(args.epochs):
-        for iter, data in enumerate(train_loader):
-
+        for i, data in enumerate(train_loader):
+            debug=True if (epoch==0 and i==0) else False
             # TODO (Q2.2): get one batch and perform forward pass
             # one batch = data for one image
             image = data['image']
@@ -145,57 +210,73 @@ def train_model(model, train_loader=None, val_loader=None, optimizer=None, args=
             wgt = data['wgt']
             rois = data['rois']
             gt_boxes = data['gt_boxes']
-            gt_class_list = data['gt_classes']
-
+            gt_class = data['gt_classes']
+            if np.any([len(r)!= 300 for r in rois]):
+                continue
+            print(np.any([len(r)!= 300 for r in rois]), [len(r) for r in rois])
             # TODO (Q2.2): perform forward pass
             # take care that proposal values should be in pixels
             # Convert inputs to cuda if training on GPU
-            
-            # forward
-            model(image, rois, gt_vec)
+            if debug: print(f"Input shapes: Image {image.size()}; ROIs:{[len(r) for r in rois]}; targets:{target.size()}")
+            cls_prob = model(image, rois, target)   # (N, 300, 20)
+            if debug: print(f"Output shape: {cls_prob.size()}")
+            # backward pass and update
             loss = model.loss
             train_loss += loss.item()
             step_cnt += 1
 
-            # backward pass and update
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-    
-            # Log to screen
-            if step % disp_interval == 0:
-                duration = t.toc(average=False)
-                fps = step_cnt / duration
-                log_text = 'step %d, image: %s, loss: %.4f, fps: %.2f (%.2fs per batch), lr: %.9f, momen: %.4f, wt_dec: %.6f' % (
-                    step, blobs['im_name'], train_loss / step_cnt, fps, 1./fps, lr, momentum, weight_decay)
-                log_print(log_text, color='green', attrs=['bold'])
-                re_cnt = True
-
-            #TODO: evaluate the model every N iterations (N defined in handout)
-            if step % eval_interval == 0 and step>0:
-                aps = test_net(name='test_weights', net = net, imdb = test_imdb, logger=logger, step=step, visualize=True, thresh = 0.0001)
-                if firstEval:
-                    vis.line(X = np.array([step]), Y = np.array([np.mean(aps)]), win="test/mAP", opts=dict(title='Test mAP'))
-                    firstEval = 0
-                else:
-                    vis.line(X = np.array([step]), Y = np.array([np.mean(aps)]), win="test/mAP", update="append", opts=dict(title='Test mAP'))
-                for i in range(len(aps)):
-                    logger.scalar_summary(tag='test/AP/'+imdb._classes[i], value=aps[i], step=step)
             
-    
+            # measure metrics and record loss
+            m1 = metric1(torch.sum(cls_prob,dim=1).cpu(), target.cpu())
+            wandb.log(
+                {'train/loss':loss, 'train/metric1': m1} #,  'train/metric2': m2,}
+            )
             # TODO (Q2.2): evaluate the model every N iterations (N defined in handout)
             # Add wandb logging wherever necessary
             if i % args.val_interval == 0 and iter != 0:
+                print("Evaluating Model.")
                 model.eval()
                 ap = test_model(model, val_loader)
-                print("AP ", ap)
                 model.train()
-
+            
             # TODO (Q2.4): Perform all visualizations here
             # The intervals for different things are defined in the handout
-            
-
+            if i % args.disp_interval==0:
+                print('Epoch: [{0}][{1}/{2}]\t'
+                  'Loss {train_loss:.4f}\t'.format(
+                      epoch,
+                      i,
+                      len(train_loader),
+                      train_loss=train_loss,
+                      # ap=ap,
+                  )
+                )
+                #logger.model_param_zhisto_summary(model=net, step=step)
+                pass
+                #
+            if i%args.disp_interval:
+                for n, im in enumerate(image):
+                    input_img = wandb.Image(im, boxes={
+                        "predictions": {
+                            "box_data": get_box_data(data['gt_classes'][n], data['gt_boxes'][n]),
+                            "class_labels": class_id_to_label,       
+                        },
+                    })
+                    # predbbox_img = wandb.Image(im, boxes={
+                    #     "predictions": {
+                    #     "box_data": get_box_data(img_info[n]['pred_boxes'], img_info[n]['pred_boxes']),
+                    #     "class_labels": class_id_to_label,       
+                    #     },
+                    # })
+                    vis_table.add_data(input_img, ap)
+                wandb.log({"train/Visuals": vis_table})
+                # wandb.log({"AP by Class": class_ap_table})
+    
     # TODO (Q2.4): Plot class-wise APs
+    
 
 
 def main():
@@ -207,7 +288,8 @@ def main():
     # TODO (Q2.2): Load datasets and create dataloaders
     dataset = VOCDataset('trainval', top_n=10, image_size=512, data_dir='../data/VOCdevkit/VOC2007/')
     n = len(dataset)
-    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [int(np.floor(n*0.8)), n-int(np.floor(n*0.8))])
+    train_dataset, val_dataset = torch.utils.data.random_split(
+        dataset, [int(np.floor(n*0.8)), n-int(np.floor(n*0.8))])
     train_sampler = torch.utils.data.SubsetRandomSampler(range(len(train_dataset)))
 
     train_loader = torch.utils.data.DataLoader(
@@ -234,8 +316,7 @@ def main():
         wandb.init(project="vlr-hw1", reinit=True)
         
     # Create network and initialize
-    net = WSDDN(classes=train_dataset.CLASS_NAMES)
-    print(net)
+    net = WSDDN(classes=dataset.CLASS_NAMES)
 
     if os.path.exists('pretrained_alexnet.pkl'):
         pret_net = pkl.load(open('pretrained_alexnet.pkl', 'rb'))
@@ -247,14 +328,12 @@ def main():
     own_state = net.state_dict()
 
     for name, param in pret_net.items():
-        print(name)
         if name not in own_state:
             continue
         if isinstance(param, Parameter):
             param = param.data
         try:
             own_state[name].copy_(param)
-            print('Copied {}'.format(name))
         except:
             print('Did not find {}'.format(name))
             continue
@@ -265,10 +344,14 @@ def main():
     net.train()
 
     # TODO (Q2.2): Freeze AlexNet layers since we are loading a pretrained model
-    for param in model.parameters():
+    for param in net.features.parameters():
         param.requires_grad = False
     # TODO (Q2.2): Create optimizer only for network parameters that are trainable
-    optimizer = None
+    params = list(net.parameters())
+    optimizer = torch.optim.SGD(params, lr=args.lr, 
+                                momentum=args.momentum, 
+                                weight_decay=args.weight_decay)
 
     # Training
+    print("Start train.")
     train_model(net, train_loader, val_loader, optimizer, args)
